409c454c8aa3d0516a7ade498ddaef5845186888
mitch-lbw <dna.fan@gmx.de>
2017-01-08 17:59:19 +0100

    initial

---
+++ organizationService/src/main/java/de/expit/jeews/endpoint/OrganizationServiceImpl.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package de.expit.jeews.endpoint;
+
+import jeews.expit.de.samples.organization.Organization;
+
+/**
+ * @since 1.4
+ * @author usimmeier
+ * @version 1.0
+ */
+public class OrganizationServiceImpl implements Organization
+{
+
+    /* (non-Javadoc)
+     * @see ws.jboss.org.samples.jmstransport.Organization#getContactInfo(java.lang.String)
+     */
+    public void getContactInfo( String arg0 )
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
